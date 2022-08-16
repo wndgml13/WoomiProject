262a1a93bd65ad8b553ac06bab4b350924eb9aac
SoJaeBeom <sojb0826@gmail.com>
2022-08-16 14:49:22 +0900

    [Feat] App.js route path 추가 및 변경

--- src/App.js
+++ src/App.js
@@ -10,18 +10,17 @@ import Header from "./components/Header";
 import PostsContainer from "./pages/PostsContainer";
 
 function App() {
-  console.log("App"); // test
   return (
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" exact element={<Main />} />
-        <Route path="detail/:id" element={<Detail />} />
-        <Route path="detail/:id" element={<AddPost />} />
-        <Route path="detail/:id" element={<EditPost />} />
-        <Route path="detail/:id" element={<Login />} />
-        <Route path="detail/:id" element={<MyPage />} />
-        <Route path="detail/:id" element={<PostsContainer />} />
+        <Route path=":boardname/detail/:id" element={<Detail />} />
+        <Route path=":boardname/addposts" element={<AddPost />} />
+        <Route path=":boardname/editposts/:id" element={<EditPost />} />
+        <Route path="login" element={<Login />} />
+        <Route path="profile" element={<MyPage />} />
+        <Route path="postscontainer/:boardname" element={<PostsContainer />} />
       </Routes>
     </BrowserRouter>
   );
